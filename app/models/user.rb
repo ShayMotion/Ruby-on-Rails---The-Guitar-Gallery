552d07e059705f60d096fd8e7d4ccd6cc31b5759
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-11-10 18:46:10 -0500

    serializers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    has_secure_password
+    belongs_to :hometown, class_name: "Location"
+    has_many :guitars 
+
+    has_many :auctions
+    has_many :brands through :guitars
+    has_many :models through :guitars
+    has_many :years through :guitars
+    has_many :prices through :guitars
+
+    validates :name, :username, :hometown, presence: true
+    validates :username, uniqueness: true
 end
