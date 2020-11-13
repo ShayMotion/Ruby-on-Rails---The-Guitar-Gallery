2b6a68ef255e7519e4e6cf2fe839974bf96da310
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-11-13 12:48:22 -0500

    views and errors

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -1,51 +1,128 @@
-class Api::V1::GuitarsController < ApplicationController
-  before_action :set_guitar, only: [:show, :update, :destroy]
+class GuitarsController < ApplicationController
 
-  # GET /guitars
   def index
-    @guitars = Guitar.all
-
-    render json: @guitars
+    if params[:auction_id] && auction = Auction.find_by_id(params[:auction_id])
+    @guitars = auction.guitars
+    @guitars = current_user.trainings.where(auction_id: params[:auction_id])
   end
 
-  # GET /guitars/1
   def show
-    render json: @guitar
+    @guitar = Guitar.find_by(id: params[:id])
   end
 
-  # POST /guitars
-  def create
+
+  def new
+    @guitar = Guitar.new 
+  end
+
+  def create 
     @guitar = Guitar.new(guitar_params)
-
     if @guitar.save
-      render json: @guitar, status: :created, location: @guitar
+    redirect_to guitar_path(@guitar)
     else
-      render json: @guitar.errors, status: :unprocessable_entity
-    end
+      render :new 
   end
 
-  # PATCH/PUT /guitars/1
-  def update
-    if @guitar.update(guitar_params)
-      render json: @guitar
-    else
-      render json: @guitar.errors, status: :unprocessable_entity
-    end
+  def edit 
+    @guitar = Guitar.find_by(id: params[:id])
   end
 
-  # DELETE /guitars/1
-  def destroy
-    @guitar.destroy
+  def update 
+    @guitar = Guitar.find_by(id: params[:id])
+    if @guitar.update)(guitar_params)
+      redirect_to guitar_path(@guitar)
+    else 
+      render :edit
+    end
+
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_guitar
-      @guitar = Guitar.find(params[:id])
-    end
+  private 
 
-    # Only allow a trusted parameter "white list" through.
-    def guitar_params
-      params.require(:guitar).permit(:name, :description)
-    end
+  def guitar_params
+    params.require(:guitar).permit(:brand, :model, :year, :price, auction_attributes:[:title, :start_date, :end_date])
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  #   before_action :set_guitar, only: [:show, :update, :destroy]
+
+#   # GET /guitars
+#   def index
+#     @guitars = Guitar.all
+
+#     render json: @guitars
+#   end
+
+#   # GET /guitars/1
+#   def show
+#     render json: @guitar
+#   end
+
+#   # POST /guitars
+#   def create
+#     @guitar = Guitar.new(guitar_params)
+
+#     if @guitar.save
+#       render json: @guitar, status: :created, location: @guitar
+#     else
+#       render json: @guitar.errors, status: :unprocessable_entity
+#     end
+#   end
+
+#   # PATCH/PUT /guitars/1
+#   def update
+#     if @guitar.update(guitar_params)
+#       render json: @guitar
+#     else
+#       render json: @guitar.errors, status: :unprocessable_entity
+#     end
+#   end
+
+#   # DELETE /guitars/1
+#   def destroy
+#     @guitar.destroy
+#   end
+
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_guitar
+#       @guitar = Guitar.find(params[:id])
+#     end
+
+#     # Only allow a trusted parameter "white list" through.
+#     def guitar_params
+#       params.require(:guitar).permit(:name, :description)
+#     end
+# end
--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -1,7 +1,21 @@
 class Guitar < ApplicationRecord
+    belongs_to :auctions
     has_many :brands, foreign_key: :guitar_id
     has_many :models
     has_many :years
     has_many :prices
     belongs_to :locations
+
+    scope :by_auction. -> (auction_id){where('auction_id= ?', auction_id}
+
+    accepts_nested_attributes_for :auction
+
+    def guitar_attributes=(guitar_params)
+        Guitar.find_or_create_by(guitar_params)
+        if guitar.valid?
+        self.guitar = guitar
+        end 
+    end
+
+     
  end
